d0191425fe396b6bd3df49cd75b9aa79832fa90a
diamondburned <diamond@arikawa-hi.me>
2023-11-06 02:58:37 -0800

    Add back generated resume.pdf

--- generate.sh
+++ generate.sh
@@ -1,26 +1,45 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-work=$(mktemp -d)
+WORK=$(mktemp -d)
+TEX_TEMPLATE="resume.tmpl.tex"
 
 main() {
-	RESUME=resume.json
-	if jq . resume.secret.json &> /dev/null; then
-		jq -s '.[0] * .[1]' resume.json resume.secret.json > "$work/resume.json"
-		RESUME="$work/resume.json"
+	generate resume.json resume.pdf &
+
+	if isValidJSON resume.secret.json; then
+		jq -s '.[0] * .[1]' \
+			resume.json \
+			resume.secret.json > "$WORK/resume.secret.json"
+
+		generate "$WORK/resume.secret.json" resume.secret.pdf &
 	fi
 
+	wait
+}
+
+# generate input.json output.pdf
+generate() {
+	local input="$1"
+	local output="$2"
+	local outputName="${output%.*}"
+
 	gomplate \
-		-c .="$RESUME" \
-		-f resume.tmpl.tex \
-		-o resume.tex \
+		-c .="$input" \
+		-o "$WORK/$outputName.tex" \
+		-f "$TEX_TEMPLATE" \
 		--left-delim '<' --right-delim '>'
 
-	texi2pdf -c -o resume.pdf -q resume.tex
+	texi2pdf -c -q "$WORK/$outputName.tex" -o "$output"
+}
+
+# isValidJSON input.json
+isValidJSON() {
+	jq . "$1" &> /dev/null
 }
 
 cleanup() {
-	rm -rf "$work"
+	rm -rf "$WORK"
 }
 
 trap cleanup EXIT
